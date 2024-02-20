eb05a80122ba540e4362c63c04f2e0779d7e3f61
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-02-20 15:45:28 -0700

    walkthough complete w/ stephen

--- server/app.py
+++ server/app.py
@@ -20,6 +20,31 @@ def index():
     body = {'message': 'Welcome to the pet directory!'}
     return make_response(body, 200)
 
+@app.route("/pets/<int:id>")
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+    # doesn't work without the '.first()'... why?
+    # pet = Pet.query.filter(Pet.id == id)
+
+    if pet:
+        body = pet.to_dict(only=("name", "species"))
+        status = 200
+    else:
+        body = {"message": f"Pet {id} not found."}
+        status = 404
+
+    return make_response(body, status)
+
+
+    
+@app.route("/pets/<string:species>")
+def pet_by_species(species):
+    pets = []
+    for pet in Pet.query.filter(Pet.species == species).all():
+        pets.append(pet.to_dict(only=("name", "species")))
+    return make_response({"count": len(pets), "pets": pets}, 200)
+    
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
